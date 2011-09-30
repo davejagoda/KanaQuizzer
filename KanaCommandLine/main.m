66a675cba001e93f301960326be4639bd32feaf2
dj <dj@djs-MacBook-Pro.local>
2011-09-30 08:10:00 -0700

    Initial Commit

---
+++ KanaCommandLine/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  KanaCommandLine
+//
+//  Created by dj on 9/30/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
