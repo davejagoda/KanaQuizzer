a5bd9ffd45a642073fd18445d6de742b37a2e2bf
Dave Jagoda <davejagoda@gmail.com>
2012-03-24 16:41:50 -0700

    commit this README

---
+++ KanaCommandLine/Kana.h
@@ -0,0 +1,13 @@
+//
+//  Kana.h
+//  KanaCommandLine
+//
+//  Created by dj on 9/30/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Kana : NSObject
+
+@end
---
+++ KanaCommandLine/Kana.m
@@ -0,0 +1,23 @@
+//
+//  Kana.m
+//  KanaCommandLine
+//
+//  Created by dj on 9/30/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Kana.h"
+
+@implementation Kana
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
